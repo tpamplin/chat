c85b8d454436048fd6d1f24a78767f40e6c6962a
tpamplin <gartiman1@gmail.com>
2025-01-22 17:18:44 -0500

    add comments to chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,21 @@
+// Import react components
 import { useEffect } from "react";
 import { StyleSheet, View, Text, ScrollView } from "react-native";
 
+// Create Chat screen component
 const Chat = ({ route, navigation }) => {
+    // Get username and background color from params from start screen
     const { name, backgroundColor } = route.params;
+    //set text color to white with dark background or black with light background
     let chatColor;
     backgroundColor === "#090C08" || backgroundColor === "#474056" ? (chatColor = "white") : (chatColor = "black");
 
+    // Set title to the username.
     useEffect(() => {
         navigation.setOptions({ title: name });
     });
 
+    // Return View with Chat.
     return (
         <View style={(styles.container, { backgroundColor })}>
             <Text style={{ color: chatColor }}>Chat!</Text>
@@ -17,6 +23,7 @@ const Chat = ({ route, navigation }) => {
     );
 };
 
+// Stylesheet
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -25,4 +32,5 @@ const styles = StyleSheet.create({
     },
 });
 
+// Export Chat screen as react component.
 export default Chat;
