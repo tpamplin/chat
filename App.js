ab224e348d67f865bf022b7a5946fcf2e4d9be33
tpamplin <gartiman1@gmail.com>
2025-01-22 01:43:09 -0500

    create start and chat screens

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
+// Import React Native components
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
+// Import Screens from components
+import Start from "./components/Start.js";
+import Chat from "./components/Chat.js";
+
+// Import Navigation Dependencies
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Create navigator Stack
+const Stack = createNativeStackNavigator();
+
+// Declare app
+const App = () => {
     return (
-        <View style={styles.container}>
-            <Text>Make sure git works!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Start">
+                <Stack.Screen name="Start" component={Start} />
+                <Stack.Screen name="Chat" component={Chat} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-}
+};
 
+// Styling
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -18,3 +34,5 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
 });
+
+export default App;
---
+++ components/Start.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+
+const Start = ({ navigation }) => {
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        navigation.setOptions({ title: "Chat App" });
+    });
+
+    return (
+        <View style={styles.container}>
+            <Text>Welcome to my chat app!</Text>
+            <TextInput
+                style={styles.textInput}
+                value={name}
+                onChangeText={setName}
+                placeholder="Enter your display name"
+            />
+            <Button title="Enter Chat" onPress={() => navigation.navigate("Chat", { name: name })} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textInput: {
+        width: "88%",
+        padding: 15,
+        borderWidth: 1,
+        marginTop: 15,
+        marginBottom: 15,
+    },
+});
+
+export default Start;
