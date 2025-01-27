f1d1f383b759d5686e9e1ce9442953cc147b865d
tpamplin <gartiman1@gmail.com>
2025-01-27 16:10:44 -0500

    add offline storage capability

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 // Import React Native components
-import { StyleSheet } from "react-native";
+import { StyleSheet, LogBox, Alert, TurboModuleRegistry } from "react-native";
+
+// Import React components
+import { useEffect } from "react";
 
 // Import Screens from components
 import Start from "./components/Start.js";
@@ -12,11 +15,29 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // Create navigator Stack
 const Stack = createNativeStackNavigator();
 
+//Import and initialize firestore.
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+
+//Import useNetInfo to allow app to know whether or not it is online
+import { useNetInfo } from "@react-native-community/netinfo";
 
 // Declare app -- setup navigation screens.
 const App = () => {
+    // Create constant which hold the App's current connection state.
+    const connectionStatus = useNetInfo();
+
+    //Alert user when internet connection is lost.
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert("Connection lost!");
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected]);
+
+    //Firebase Configuration
     const firebaseConfig = {
         apiKey: "AIzaSyAGMyXQZPxQ7SU9yXvu6I9APD4RMZm8JMs",
         authDomain: "chat-app-pamplin.firebaseapp.com",
@@ -26,14 +47,18 @@ const App = () => {
         appId: "1:995725146726:web:882d61d0dd9af228eb4ba7",
     };
 
+    //Connect to firebase and define database.
     const app = initializeApp(firebaseConfig);
-
     const db = getFirestore(app);
+
+    //React-native navigation handling.
     return (
         <NavigationContainer style={styles.container}>
             <Stack.Navigator initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
-                <Stack.Screen name="Chat">{(props) => <Chat db={db} {...props} />}</Stack.Screen>
+                <Stack.Screen name="Chat">
+                    {(props) => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}
+                </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     );
--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,18 @@
 // Import react components
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import { StyleSheet, View, Text, ScrollView, KeyboardAvoidingView, Platform, Alert } from "react-native";
+
+//Import Firebase components, so the app can access the firestore database.
 import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 
-// Import Gifted Chat
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+// Import GiftedChat to handle the chat UI
+import { Bubble, InputToolbar, GiftedChat } from "react-native-gifted-chat";
+
+//Import AsyncStorage so messages can be stored and viewed offline
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Create Chat screen component
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
     // Get username, user ID and background color from params from start screen
     const { name, backgroundColor, userID } = route.params;
 
@@ -19,28 +24,60 @@ const Chat = ({ route, navigation, db }) => {
         addDoc(collection(db, "messages"), newMessages[0]);
     };
 
-    //Initialize state
+    //Store messages in AsyncStorage.
+    const storeMessages = async (messagesToStore) => {
+        try {
+            await AsyncStorage.setItem("oldMessages", JSON.stringify(messagesToStore));
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    //Load messages from AsyncStorage
+    const loadStoredMessages = async () => {
+        const storedMessages = (await AsyncStorage.getItem("oldMessages")) || [];
+        setMessages(JSON.parse(storedMessages));
+    };
+
+    //create onSnapshot listener to listen to the messages collection in db, and update messages array whenever there is a change.
+    let unsubMessages;
     useEffect(() => {
-        // Set title to the username.
+        //set page title to user's username
         navigation.setOptions({ title: name });
-    }, []);
 
-    // Set Default test messages
-    useEffect(() => {
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-        const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
-            let newMessages = [];
-            documentsSnapshot.forEach((doc) => {
-                newMessages.push({ id: doc.id, ...doc.data(), createdAt: new Date(doc.data().createdAt.toMillis()) });
+        if (isConnected === true) {
+            // Deregister current onSnapshot listener to avoid having multiple running concurrently, since useEffect will be called whenever there is a change in connection status.
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+
+            //define the query location so onSnapshot knows where to look for updates
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+
+            // Load messages from firestore database whenever the database is updated
+            unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+                let newMessages = [];
+                documentsSnapshot.forEach((doc) => {
+                    // Adds new messages to the newMessages array in the order that they were created.
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis()),
+                    });
+                });
+
+                //store messages in AsyncStorage and update messages array with new messages.
+                storeMessages(newMessages);
+                setMessages(newMessages);
             });
-            setMessages(newMessages);
-        });
+        } else loadStoredMessages();
 
+        //Clean up code!
         return () => {
             if (unsubMessages) unsubMessages();
         };
-    }, []);
+    }, [isConnected]);
 
+    //Changes the color of chat bubbles based on if they are from the current user (right) or a different user (left)
     const renderBubble = (props) => {
         return (
             <Bubble
@@ -57,12 +94,19 @@ const Chat = ({ route, navigation, db }) => {
         );
     };
 
+    //only render toolbar if connection is online
+    const renderInputToolbar = (props) => {
+        if (isConnected === true) {
+            return <InputToolbar {...props} />;
+        } else return null;
+    };
     // Return View with Chat.
     return (
         <View style={[styles.container, { backgroundColor: backgroundColor }]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
                 onSend={(messages) => onSend(messages)}
                 user={{
                     _id: userID,
